d240de17f02733f6adacdaceb933538f0ef89ec2
Sir-Jarvis <sirjarvis@anything.com>
2021-12-23 01:26:04 +0000

    last deployment commit

--- gestion_institution/settings.py
+++ gestion_institution/settings.py
@@ -126,9 +126,7 @@ STATIC_URL = '/static/'
 # )
 
 # Extra lookup directories for collectstatic to find static files
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
@@ -151,9 +149,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATICFILES_DIRS = [(os.path.join(BASE_DIR, 'static'),)]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
